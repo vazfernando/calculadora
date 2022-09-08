e76c09b0da27a8d272d8c7fb29de443c26d7c18f
Fernando Vaz <fernandovazz@gmail.com>
2022-09-08 19:59:17 -0300

    update

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,3 +1,12 @@
-function operacoes() {
-    var  = 
+var button7 = Number(window.document.getElementById('button7'))
+var button7 = 7
+var plus = window.document.getElementById('buttonplus')
+var plus = +
+
+function pressresult() {
+
+}
+
+function signs(){
+
 }
